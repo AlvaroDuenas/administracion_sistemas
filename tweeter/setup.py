166bb14d8b8ceedc855909c087c3ee296f1064a7
Alvaro Dueñas <alvaro25duenas@gmail.com>
2020-11-20 17:04:42 +0100

    Inicialización Modulo Twetter

---
+++ tweeter/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tweeter',
+    version='1.0',
+    packages=find_packages(),
+    url='https://github.com/AlvaroDuenas/administracion_sistemas',
+    license='',
+    author='alvaro',
+    author_email='alvaro25duenas@gmail.com',
+    description='Gestion de api tweeter'
+)
